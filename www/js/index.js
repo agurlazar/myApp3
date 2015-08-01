311601e725a481c6b07869ab4e27b590ff1520e0
Mohd Azhar Dris <m.azhardris@gmail.com>
2015-08-02 05:58:46 +0800

    add camera function

--- www/js/index.js
+++ www/js/index.js
@@ -49,7 +49,18 @@ var app = {
         console.log('Received Event: ' + id);
     },
     fnCamera: function(){
+        navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+            destinationType: Camera.DestinationType.DATA_URL
+        });
 
+        function onSuccess(imageData) {
+            var image = document.getElementById('myImage');
+            image.src = "data:image/jpeg;base64," + imageData;
+        }
+
+        function onFail(message) {
+            alert('Failed because: ' + message);
+        }
     },
     fnScan: function(){
         cordova.plugins.barcodeScanner.scan(
