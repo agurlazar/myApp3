7f4624f56b30a0c9badb5f501482bafa809397a6
Mohd Azhar Dris <m.azhardris@gmail.com>
2015-08-07 06:22:19 +0800

    -

--- www/js/index.js
+++ www/js/index.js
@@ -51,7 +51,7 @@ var app = {
       tx.executeSql("select data, data_num as cnt from test_table;", [], function(tx, res) {
         alert("res.rows.length: " + res.rows.length + " -- should be 1");
         alert("res.rows.item(0).data: " + res.rows.item(0).data + " -- should be test");
-          alert("res.rows.item(0).data_num: " + res.rows.item(0).data_num + " -- should be 100");
+          alert("res.rows.item(1): " + res.rows.item(1)+ " -- should be 100");
 
       });
 
