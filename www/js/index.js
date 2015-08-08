b3f20f6087d223cdaf80010b49abd51885f71af7
Mohd Azhar Dris <m.azhardris@gmail.com>
2015-08-08 13:54:54 +0800

    try webdatabase

--- www/js/index.js
+++ www/js/index.js
@@ -37,9 +37,28 @@ var app = {
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
         alert("test name item ");
+        var dbSize = 5 * 1024 * 1024; // 5MB
+        html5rocks.webdb.db = openDatabase("Todo", "1", "Todo manager", dbSize);
 
+        html5rocks.webdb.db.transaction(function(tx) {
+    tx.executeSql('DROP TABLE IF EXISTS test_table');
+    tx.executeSql('CREATE TABLE IF NOT EXISTS test_table (id integer primary key, data text, data_num integer)');
+
+    tx.executeSql("INSERT INTO test_table (data, data_num) VALUES (?,?)", ["test", 100], function(tx, res) {
+      console.log("insertId: " + res.insertId + " -- probably 1");
+      console.log("rowsAffected: " + res.rowsAffected + " -- should be 1");
+
+      tx.executeSql("select count(id) as cnt from test_table;", [], function(tx, res) {
+        console.log("res.rows.length: " + res.rows.length + " -- should be 1");
+        console.log("res.rows.item(0).cnt: " + res.rows.item(0).cnt + " -- should be 1");
+      });
+
+    }, function(e) {
+      console.log("ERROR: " + e.message);
+    });
+  });
+/*
          var db = window.sqlitePlugin.openDatabase({name: "my.db", androidDatabaseImplementation: 2});
-
   db.transaction(function(tx) {
     tx.executeSql('DROP TABLE IF EXISTS test_table');
     tx.executeSql('CREATE TABLE IF NOT EXISTS test_table (id integer primary key, datas text, data_num integer, trythis text)');
@@ -61,7 +80,7 @@ var app = {
     });
   });
 
-
+*/
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
